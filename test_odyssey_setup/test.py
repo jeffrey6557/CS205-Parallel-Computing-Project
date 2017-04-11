dba4d37f8ba2b8561a15ca278c2efeac829a6de6
Linglin Huang <lhuang1@andrew.cmu.edu>
2017-04-11 15:24:10 -0400

    commit before merge

--- test_odyssey_setup/test.py
+++ test_odyssey_setup/test.py
@@ -4,46 +4,38 @@ import time
 import omp_test
 from mpi4py import MPI
 
-TaskMaster = 0
-comm = MPI.COMM_WORLD
-rank = comm.Get_rank()
+nproc = MPI.COMM_WORLD.Get_size()   # Size of communicator 
+iproc = MPI.COMM_WORLD.Get_rank()   # Ranks in communicator
+inode = MPI.Get_processor_name()    # Node where this MPI process runs
+
+if iproc == 0: print "This code is a test for mpi4py."
+
+for i in range(0,nproc):
+    MPI.COMM_WORLD.Barrier()
+    if iproc == i:
+        print '1. Rank %d out of %d' % (iproc,nproc)
+
+MPI.COMM_WORLD.Barrier()
+
+print '2. Rank %d out of %d' % (iproc,nproc)
+
+MPI.COMM_WORLD.Barrier()
 n = 2**8
+X = np.random.random(size=(n, n))
+Y = np.random.random(size=(n, n))
+Z_seq = np.zeros((n, n))
+start = time.time()
+Z_seq = np.array(omp_test.mmult_seq(X, Y, Z_seq))
+time_seq = time.time() - start
+Z_par = np.zeros((n, n))
+start = time.time()
+Z_par = np.array(omp_test.mmult_par(X, Y, Z_par))
+time_par = time.time() - start
 
-if rank == TaskMaster:
-	X = np.random.random(size=(n, n))
-	Y = np.random.random(size=(n, n))
-	Z_seq = np.zeros((n, n))
-	Z_mpi = np.zeros((n, n))
-
-	start = time.time()
-	Z_seq = np.array(omp_test.mmult_seq(X, Y, Z_seq))
-	time_seq = time.time() - start
-
-	start = time.time()
-
-rank = comm.Get_rank()
-size = comm.Get_size()
-sub_n = int(np.ceil((n+0.0)/size)) #number of rows for each process
-
-# broad cast Y to every process
-comm.bcast(Y, root=TaskMaster)
-
-# scatter rows of X and Z
-sub_X = np.empty([sub_n, n], dtype=np.float)
-comm.Scatter(X, sub_X, root=TaskMaster)
-
-# create empty matrix to store results
-sub_Z = np.zeros((sub_n, n))
-sub_Z = np.array(omp_test.mmult_par(sub_X, Y, sub_Z))
-
-# gather results from all processes
-comm.Gather(sub_Z, Z_mpi, root=TaskMaster)
-
-if rank == TaskMaster:
-	time_par = time.time() - start
-	print('PAR Correct? ', np.allclose(Z_seq, Z_mpi, rtol=1e-03, atol=1e-05))
-	print(time_seq, time_par, 'speedup: ', time_seq/time_par)
-
-
+print '3. Rank %d out of %d' % (iproc,nproc)
+print 'PAR Correct? ', np.allclose(Z_seq, Z_par, rtol=1e-03, atol=1e-05)
+print time_seq, time_par, 'speedup: ', time_seq/time_par 
+        
+MPI.Finalize()
 
 
