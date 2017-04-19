4f62842a244bdc1613e9fc7bb7e2c3b005f83d6d
kpoels <kpoels@g.harvard.edu>
2017-04-18 20:14:14 -0400

    Updated some files

--- comparison_plots.R
+++ comparison_plots.R
@@ -1,3 +1,5 @@
+library(ggplot2)
+
 # Cuda
 cuda_loss = c(1.3733631402355129, 0.93689111362069344, 0.91562216390997675, 0.90943513039815227, 0.90553493915978123, 0.90839237429327879, 0.89844868593296767, 0.89804859597804187, 0.89566163630808815, 0.89294650195008618, 0.89129256531343626, 0.89407325882022659, 0.89177121463468523, 0.88653495932029469, 0.88972305944410424, 0.89187817670531189, 0.88975714647163784, 0.89168526544409288, 0.89036472449868409, 0.88598235974877571)
 cuda_valLoss = c(0.80806828995390856, 0.79556096959461486, 0.78946638959660365, 0.78453055065553179, 0.78353461688835713, 0.78164274223535113, 0.78354178043350609, 0.78122799726148173, 0.78135497052562075, 0.78002393609263132, 0.77844687327818318, 0.77849215448851994, 0.77751679577612542, 0.77562201441202838, 0.77743307886104263, 0.77637484535367884, 0.77685708831585454, 0.77796572454902368, 0.77658879340846265, 0.77585375844240589)
@@ -16,7 +18,7 @@ cuda_loss = c(cuda_loss, rep(NA,6)); cuda_valLoss = c(cuda_valLoss, rep(NA,6))
 mp_loss = c(mp_loss, rep(NA,12)); mp_valLoss = c(mp_valLoss, rep(NA,12))
 
 ### Plot training loss
-pdf("trainingLoss_plot.pdf", width = 3.5, height = 3, pointsize = .7)
+png("images/trainingLoss_plot.png")
 plot(epochs, seq_loss, type = "l", lwd = 2, ylim = c(min(cuda_loss, seq_loss, mp_loss, na.rm = T), 
                                                      max(cuda_loss, seq_loss, mp_loss, na.rm = T)),
      xlab = "Number of Epoch", ylab = "Training Loss")
@@ -26,7 +28,7 @@ legend("topright", legend = c("Sequential","Cuda","OpenMP"), col = 1:3, lwd = 2)
 dev.off()
 
 ### Plot validation loss
-pdf("validationLoss_plot.pdf", width = 3.5, height = 3, pointsize = .7)
+png("images/validationLoss_plot.png")
 plot(epochs, seq_valLoss, type = "l", lwd = 2, ylim = c(min(cuda_valLoss, seq_valLoss, mp_valLoss, na.rm = T), 
                                                      max(cuda_valLoss, seq_valLoss, mp_valLoss, na.rm = T)),
      xlab = "Number of Epoch", ylab = "Validation Loss")
@@ -57,7 +59,7 @@ interval1 <- -qnorm((1-0.9)/2)  # 90% multiplier
 interval2 <- -qnorm((1-0.95)/2)  # 95% multiplier
 
 # Plot
-pdf("error_metrics.pdf", width = 6, height = 6, pointsize = .7)
+png("images/error_metrics.png")
 zp1 <- ggplot(allModelFrame, aes(colour = Algorithm))
 zp1 <- zp1 + geom_hline(yintercept = 0, colour = gray(1/2), lty = 2)
 zp1 <- zp1 + geom_linerange(aes(x = Metrics, ymin = Estimate - SE*interval1/interval2,
